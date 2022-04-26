66221898a2dec2f7c3f355cf39c30395d14d7161
judevercetti <judevercetti@gmail.com>
2022-04-26 14:27:52 +0300

    fixed responsiveness

--- sadaka/HTML/public/script.js
+++ sadaka/HTML/public/script.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "answerzcry",
   storageBucket: "answerzcry.appspot.com",
   messagingSenderId: "420113336306",
-  appId: "1:420113336306:web:a3877f53088f9e259b4d6b"
+  appId: "1:420113336306:web:a3877f53088f9e259b4d6b",
 };
 
 // Initialize Firebase
@@ -21,9 +21,11 @@ db = firebase.firestore();
 
 // login
 function login() {
-  email = document.getElementById('mail').value;
-  password = document.getElementById('pass').value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  email = document.getElementById("mail").value;
+  password = document.getElementById("pass").value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Signed in
       var user = userCredential.user;
@@ -33,95 +35,88 @@ function login() {
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
-      alert(errorMessage)
+      alert(errorMessage);
       window.location.href = "about.html";
     });
 }
-
 
 // state observer to prevsent non admins  fron accessing the dashboard
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     uid = user.uid;
-    console.log(uid)
+    console.log(uid);
     // User is signed in.
   } else {
     if (window.location.pathname == "/dashboard.html") {
-
       window.location.href = "../index.html";
     }
     // No user is signed in.
   }
 });
 
-
-// upload post 
+// upload post
 
 function upload_to_db(imageURL) {
-
-  db.collection("Posts").add({
-    heading: document.getElementById('head').value,
-    main: document.getElementById('main').value,
-    postDate: document.getElementById('dateid').value,
-    item_image: imageURL,
-
-  })
+  db.collection("Posts")
+    .add({
+      heading: document.getElementById("head").value,
+      main: document.getElementById("main").value,
+      postDate: document.getElementById("dateid").value,
+      item_image: imageURL,
+    })
     .then((docRef) => {
       console.log("Document written with ID: ", docRef.id);
-      alert('success');
+      alert("success");
     })
     .catch((error) => {
-      alert('fail' + errorMessage);
+      alert("fail" + errorMessage);
 
       console.error("Error adding document: ", error);
     });
 }
 
-
 function post_news() {
-  var file = document.getElementById('pic').files[0]
+  var file = document.getElementById("pic").files[0];
 
   var storageRef = firebase.storage().ref(file.name);
   var task = storageRef.put(file);
-  task.on('state_changed', function progress(snapshot) {
-    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log(percentage + "%");
-
-  }, function error(err) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("failed to upload" + errorMessage);
-
-  }, function complete() {
-    console.log("image uploaded");
-    task.snapshot.ref.getDownloadURL().then((downloadURL) => {
-      // do logic with downloadURL
-      upload_to_db(downloadURL);
-    });
-  });
+  task.on(
+    "state_changed",
+    function progress(snapshot) {
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(percentage + "%");
+    },
+    function error(err) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("failed to upload" + errorMessage);
+    },
+    function complete() {
+      console.log("image uploaded");
+      task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        // do logic with downloadURL
+        upload_to_db(downloadURL);
+      });
+    }
+  );
 }
-
 
 // populating posts table
 function read_posts() {
+  db.collection("Posts").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      product = doc.data();
+      // regrence to your table
+      table_ref = document
+        .getElementById("posts_table")
+        .getElementsByTagName("tbody")[0];
+      // inser row after the last index always
+      newRow = table_ref.insertRow(-1);
 
-
-  db.collection("Posts")
-
-    .onSnapshot((querySnapshot) => {
-
-      querySnapshot.forEach((doc) => {
-        product = doc.data();
-        // regrence to your table
-        table_ref = document.getElementById('posts_table').getElementsByTagName('tbody')[0];
-        // inser row after the last index always
-        newRow = table_ref.insertRow(-1);
-
-
-        newRow.innerHTML = `<tr> 
+      newRow.innerHTML = `<tr> 
        
          <td> ${product["heading"]}</td>
-         <td><img src="${product['item_image']}" height=50></td>
+         <td><img src="${product["item_image"]}" height=50></td>
          <td>${product["main"]}</td>
          <td>                  
            <button class="btn" onClick="delete_post('${doc.id}')">Delete</button>
@@ -131,29 +126,26 @@ function read_posts() {
        </td>
        <td>${product["postDate"]}</td>
          <td>  
-        </tr>`
-
-
-      });
+        </tr>`;
     });
+  });
 }
 
-
-
-
-// reading postst 
+// reading postst
 function display_posts() {
-  db.collection("Posts")
-    .onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        product = doc.data();
-        div_ref = document.getElementById('posts');
+  db.collection("Posts").onSnapshot((querySnapshot) => {
+    var posts = ``;
+    querySnapshot.forEach((doc) => {
+      product = doc.data();
+      div_ref = document.getElementById("posts");
 
-        div_ref.innerHTML += `<img src="${product['item_image']} alt="image" class="cause-img"style="height: 100%; width:100%;">
+      posts += `<div class="col-md-3 col-sm-6">
+      
+        <img src="${product["item_image"]} alt="image" class="cause-img"style="height: 100%; width:100%;">
 
         <div class="progress cause-progress">
           <div>
-          ${product['postDate']} 
+          ${product["postDate"]} 
           
             </div>
         </div>
@@ -170,135 +162,110 @@ function display_posts() {
         </div>
         <hr>
         <br>
-
-
-    `
-          ;
-
-      });
-    })
-
+        </div>
+    `;
+    });
+    div_ref.innerHTML = posts;
+  });
 }
-
-
-
 
 // posting advertisement
 
 function upload_to_db_ad(imageURL) {
-
-  db.collection("advertisemnts").add({
-    caption: document.getElementById('caption').value,
-    // main:document.getElementById('main').value,
-    item_image: imageURL,
-
-  })
+  db.collection("advertisemnts")
+    .add({
+      caption: document.getElementById("caption").value,
+      // main:document.getElementById('main').value,
+      item_image: imageURL,
+    })
     .then((docRef) => {
       console.log("Document written with ID: ", docRef.id);
-      alert('success');
+      alert("success");
     })
     .catch((error) => {
-      alert('fail' + errorMessage);
+      alert("fail" + errorMessage);
 
       console.error("Error adding document: ", error);
     });
 }
 
-
 function post_ad() {
-  var file = document.getElementById('ad_pic').files[0]
+  var file = document.getElementById("ad_pic").files[0];
 
   var storageRef = firebase.storage().ref(file.name);
   var task = storageRef.put(file);
-  task.on('state_changed', function progress(snapshot) {
-    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log(percentage + "%");
-
-  }, function error(err) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("failed to upload" + errorMessage);
-
-  }, function complete() {
-    console.log("image uploaded");
-    task.snapshot.ref.getDownloadURL().then((downloadURL) => {
-      // do logic with downloadURL
-      upload_to_db_ad(downloadURL);
-    });
-  });
+  task.on(
+    "state_changed",
+    function progress(snapshot) {
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(percentage + "%");
+    },
+    function error(err) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("failed to upload" + errorMessage);
+    },
+    function complete() {
+      console.log("image uploaded");
+      task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        // do logic with downloadURL
+        upload_to_db_ad(downloadURL);
+      });
+    }
+  );
 }
-
-
-
-
 
 // populating Ads table
 function read_ads() {
+  db.collection("advertisemnts").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      product = doc.data();
+      // regrence to your table
+      table_ref = document
+        .getElementById("ads_table")
+        .getElementsByTagName("tbody")[0];
+      // inser row after the last index always
+      newRow = table_ref.insertRow(-1);
 
-
-  db.collection("advertisemnts")
-
-    .onSnapshot((querySnapshot) => {
-
-      querySnapshot.forEach((doc) => {
-        product = doc.data();
-        // regrence to your table
-        table_ref = document.getElementById('ads_table').getElementsByTagName('tbody')[0];
-        // inser row after the last index always
-        newRow = table_ref.insertRow(-1);
-
-
-        newRow.innerHTML = `<tr> 
+      newRow.innerHTML = `<tr> 
        
          <td> ${product["caption"]}</td>
-         <td><img src="${product['item_image']}" height=50></td>
+         <td><img src="${product["item_image"]}" height=50></td>
          <td>          
            <button class="btn" onClick="delete_items('${doc.id}')">Delete</button>
          </td>
          <td>          
          <button class="btn" onClick="edit_itens('${doc.id}')">Edit </button>
        </td>
-        </tr>`
-
-
-      });
+        </tr>`;
     });
+  });
 }
-
-
 
 // reading advertisement
 function display_ads() {
-  db.collection("advertisemnts")
-    .onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        product = doc.data();
-        div_ref = document.getElementById('slide_showBucket');
-        console.log(product);
-        div_ref.innerHTML += `
-       <div class="mySlides fade">
-  <img src="${product.item_image}" style="width:100%">
-  <div class="text">${product.caption}</div>
-</div>
-       `
-        document.getElementById('dots').innerHTML += `
-  <span class="dot"></span> 
+  db.collection("advertisemnts").onSnapshot((querySnapshot) => {
+    div_ref = document.getElementById("slide_showBucket");
+    div_ref.innerHTML += ``;
 
-  `
+    querySnapshot.forEach((doc) => {
+      product = doc.data();
+      console.log(product);
+      div_ref.innerHTML += `
+            <div class="mySlides fade">
+            <img src="${product.item_image}" style="width:100%">
+            <div class="text">${product.caption}</div>
+            </div>`;
 
-
-      });
-      showSlides();
-    })
-
+      document.getElementById("dots").innerHTML += `<span class="dot"></span>`;
+    });
+    showSlides();
+  });
 }
 
-
-//  slide shw function
+//  slide show function
 
 let slideIndex = 0;
-// showSlides();
-
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -307,40 +274,43 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
-
 // delete adverts
 function delete_items(id) {
-  db.collection("advertisemnts").doc(id).delete().then(() => {
-    console.log("Document successfully deleted!");
-  }).catch((error) => {
-    console.error("Error removing document: ", error);
-  });
-
+  db.collection("advertisemnts")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log("Document successfully deleted!");
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
 }
-
-
-
-
 
 // delete posts
 function delete_post(id) {
-  db.collection("Posts").doc(id).delete().then(() => {
-    console.log("Document successfully deleted!");
-  }).catch((error) => {
-    console.error("Error removing document: ", error);
-  });
-
+  db.collection("Posts")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log("Document successfully deleted!");
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
 }
-
 
 // Flutterwave
 function donate() {
@@ -351,14 +321,14 @@ function donate() {
   FlutterwaveCheckout({
     public_key: "FLWPUBK-fe9c6680efd0ded2fa586ccad267ecec-X",
     tx_ref: "hey",
-    amount: document.getElementById('amount').value,
-    currency: document.getElementById('currency').value,
-    country: document.getElementById('country').value,
+    amount: document.getElementById("amount").value,
+    currency: document.getElementById("currency").value,
+    country: document.getElementById("country").value,
     payment_options: "mobilemoneyuganda",
     redirect_url: "https://answerthecry.org/index.html",
 
     customer: {
-      email: document.getElementById('email').value,
+      email: document.getElementById("email").value,
       // phone_number: "",
       // name: user.displayName,
     },
@@ -376,12 +346,3 @@ function donate() {
     },
   });
 }
-
-
-
-
-
-
-
-
-
